658c381444ea316f7373642d79d0f8fa5895999b
noramohamsa <nora.mohamed231@gmail.com>
2024-08-03 15:48:43 +0300

    update at brand name syntax

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,6 +8,7 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
